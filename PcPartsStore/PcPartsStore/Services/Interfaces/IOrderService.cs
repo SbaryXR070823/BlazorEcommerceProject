0a7abf014d2bf73a9e115d89a5dc413e6940fdf4
Ionut-Alexandru Sbaroi <sbaroialex@gmail.com>
2025-03-05 19:32:53 +0200

    Missing functionality partial implementation
    - implemented quantity update for the cart items
    - implemented the checkout functionality only visually
    - started the Order Service

---
+++ PcPartsStore/PcPartsStore/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Models;
+
+namespace PcPartsStore.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetOrdersAsync();
+        Task<Order> GetOrderByIdAsync(int id);
+        Task AddOrderAsync(Order order);
+    }
+}
---
+++ PcPartsStore/PcPartsStore/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using PcPartsStore.Services.Interfaces;
+using PcPartsStore.UnitOfWork;
+using Shared.Models;
+
+namespace PcPartsStore.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<Order>> GetOrdersAsync()
+        {
+            return (await _unitOfWork.Orders.GetAllAsync()).ToList();
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int id)
+        {
+            return await _unitOfWork.Orders.GetByIdAsync(id);
+        }
+
+        public async Task AddOrderAsync(Order order)
+        {
+            await _unitOfWork.Orders.AddAsync(order);
+        }
+
+    }
+}
