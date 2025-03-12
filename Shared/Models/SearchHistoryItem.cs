46679a5f6bed661653993e2652eca989bc62669e
Ionut Alexandru Sbaroi <sbaroialex@gmail.com>
2025-03-12 18:54:02 +0200

    Predictive searching - added the functionality of predictive searching - now everytime the user apply the filters and it contains a value inside the searchTerm filed it will be stored in local storage together with the timestamp of the insert for ordering

---
+++ Shared/Models/SearchHistoryItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Models
+{
+    public class SearchHistoryItem
+    {
+        public string Term { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
--- Shared/Services/LocalStorageService.cs
+++ Shared/Services/LocalStorageService.cs
@@ -1,43 +1,42 @@
 ﻿using Blazored.LocalStorage;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Shared.Services;
-
-public class LocalStorageService
+namespace Shared.Services
 {
-    private readonly ILocalStorageService _localStorage;
-
-    public LocalStorageService(ILocalStorageService localStorage)
+    public class LocalStorageService
     {
-        _localStorage = localStorage;
-    }
+        private readonly ILocalStorageService _localStorage;
 
-    public async Task SaveItemAsync<T>(string key, T value)
-    {
-        await _localStorage.SetItemAsync(key, value);
-    }
+        public LocalStorageService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
 
-    public async Task<T?> GetItemAsync<T>(string key)
-    {
-        return await _localStorage.GetItemAsync<T>(key);
-    }
+        public async Task SaveItemAsync<T>(string key, T value)
+        {
+            await _localStorage.SetItemAsync(key, value);
+        }
 
-    public async Task RemoveItemAsync(string key)
-    {
-        await _localStorage.RemoveItemAsync(key);
-    }
+        public async Task<T?> GetItemAsync<T>(string key)
+        {
+            return await _localStorage.GetItemAsync<T>(key);
+        }
 
-    public async Task<bool> ContainsKeyAsync(string key)
-    {
-        return await _localStorage.ContainKeyAsync(key);
-    }
+        public async Task RemoveItemAsync(string key)
+        {
+            await _localStorage.RemoveItemAsync(key);
+        }
 
-    public async Task ClearAllAsync()
-    {
-        await _localStorage.ClearAsync();
+        public async Task<bool> ContainsKeyAsync(string key)
+        {
+            return await _localStorage.ContainKeyAsync(key);
+        }
+
+        public async Task ClearAllAsync()
+        {
+            await _localStorage.ClearAsync();
+        }
     }
 }
